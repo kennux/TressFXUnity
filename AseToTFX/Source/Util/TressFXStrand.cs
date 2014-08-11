569888ac78e5c3af3ba5c0b86f48962a27b1180d
KennuX <kennux@ymail.com>
2014-08-11 18:33:08 +0200

    Added AseToTFX tool

---
+++ AseToTFX/Source/Util/TressFXStrand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AseToTFX.Util
+{
+    public class TressFXStrand
+    {
+        public Vector3[] vertices;
+        public float texcoordX;
+    }
+}
---
+++ AseToTFX/Source/Util/Vector3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AseToTFX.Util
+{
+    /// <summary>
+    /// Very simple vector3 implementation
+    /// </summary>
+    public struct Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public static Vector3 zero
+        {
+            get { return new Vector3(0, 0, 0); }
+        }
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
